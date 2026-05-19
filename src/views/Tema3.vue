a6ddf8a5235550cbe3753198a4c49a2b8520cdf3
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-05-18 23:52:20 -0500

    Tema3 parte 1

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,151 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Tema 3
+      h1 Información turística
+
+    .mt-5.pt-4
+      .fullwidth-bg.fullwidth-bg--1
+      img(data-aos="fade-up")(src="@/assets/curso/tema3/1.png", alt="").z-2
+      
+      .row.justify-content-center.z-2.mb-3
+        .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+          div.banner-text.shadow
+            p.mb-0 La información turística es inherente al desarrollo de la actividad. Un visitante siempre necesitará de una guía confiable para que su experiencia en el destino sea placentera y exitosa; más aún los viajeros que deciden emprender los recorridos por su propia cuenta. Según ONU Turismo, la información turística corresponde a:
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/2.png", alt="")
+      .col-lg-8
+        p Aquellos servicios públicos, dependientes por regla general de organismos públicos o instituciones, que tienen como misión informar, facilitar y orientar al turista durante su estancia vacacional o viajes, facilitando gratuitamente información. (ONU Turismo, s. f.)
+        .p-4.bg-acento-contenido-a5.rounded-4
+          p.mb-0 Es conveniente diferenciar la información turística de otras actividades relacionadas, como la promoción y la comunicación turística. En ese sentido, la característica más relevante que debe tener la información turística — y que constituye su principal diferenciador frente a las otras actividades mencionadas — es ser imparcial.
+
+    Separador
+
+    #t_3_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 3.1 Características del informador turístico 
+
+    .p-4.bg-acento-contenido-a5.rounded-4.mb-4
+      p.mb-0 El informador turístico cumple funciones de transmisión e interacción con el visitante.
+    
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/3.png", alt="")
+      .col-lg-8
+        p El informador turístico es por excelencia el rostro del destino. Se requiere que, además de dominar el conocimiento respecto de un destino o atractivo turístico particular, maneje técnicas básicas de comunicación y expresión, así como herramientas informáticas.
+        p.mb-0 Por tanto, debe ser una persona con facilidad de expresión verbal y con rasgos de presentación y pulcritud que transmitan confianza al viajero. También son necesarias habilidades documentales básicas que permitan recolectar información aplicando métodos de investigación cuantitativos y cualitativos desde medios impresos o digitales.
+
+    .bg-title.bg-title--lg.p-3.rounded-5.pe-5.mb-4
+      h5.mb-0 Naturaleza de la información turística
+
+    .p-4.bg-acento-contenido-a5.rounded-4.mb-4
+      p.mb-0 La información turística debe suministrar contenidos neutrales sobre los recursos y los destinos, y buscar que el turista valore la información recibida y pueda tomar sus propias decisiones. Para lograr este objetivo, la información:
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/4.png", alt="")
+      .col-lg-8
+        .rounded-4.bg-img.bg-img--8.p-4.h-100
+          .row.align-items-center.mb-4
+            .col-12
+              ul.lista-ul--color.pe-4.m-0
+                li.d-flex.mb-0
+                  i.fas.fa-minus.fa-xs.mt-2
+                  span Debe transmitirse de forma clara.
+                li.d-flex.mb-0
+                  i.fas.fa-minus.fa-xs.mt-2
+                  span No debe tomar partido por ningún servicio en particular.
+                li.d-flex.mb-0
+                  i.fas.fa-minus.fa-xs.mt-2
+                  span Debe ir acompañada de folletos, planos y guías, entre otros, editados y distribuidos por instituciones oficiales. 
+                li.d-flex.mb-0
+                  i.fas.fa-minus.fa-xs.mt-2
+                  span Debe distribuirse en las lenguas más comunes.
+                li.d-flex.mb-0
+                  i.fas.fa-minus.fa-xs.mt-2
+                  span Debe acompañarse de una actitud no verbal adecuada y amable.
+
+    .bg-title.p-3.rounded-5.pe-5.mb-4
+      h5.mb-0 Perfil del informador
+
+    p Se han destacado las características de la comunicación turística; ahora es necesario considerar las cualidades y habilidades del personal que transmitirá dicha información, pues son el componente principal en la interacción con el cliente. En el contexto hotelero, este rol recae específicamente en los empleados de recepción: recepcionistas y conserjes.
+    .p-4.bg-acento-botones-a5.rounded-4.mb-4
+      p.mb-0 Los contenidos pueden estar bien elaborados y enfocados, pero si la persona que los transmite no logra generar empatía con el turista, la experiencia será negativa. Para lograrlo, los empleados con funciones de atención al cliente deben prestar atención a los siguientes aspectos:
+    
+    .row.justify-content-center.align-items-stretch.z-2
+      .col-lg-4.mb-4.mb-lg-0
+        .p-4.rounded-4.bg-img.bg-img--10.h-100
+          ul.lista-ul--color.pe-4.m-0
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Pensar antes de hablar.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Preferir informar con precisión y claridad.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Dosificar la información.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Preguntar.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Escuchar activamente.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Ser asertivo.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Adaptar el mensaje de acuerdo con las características de la demanda.
+      .col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/5.png", alt="")
+      .col-lg-4
+        .p-4.rounded-4.bg-img.bg-img--10.h-100
+          ul.lista-ul--color.pe-4.m-0
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Evitar ruidos.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Tener una postura corporal adecuada.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Mantener el contacto visual, sin intimidar a la contraparte.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Sonreír.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Mantener un espacio personal óptimo.
+            li.d-flex.mb-0
+              i.fas.fa-minus
+              span Fijarse de los elementos paralingüísticos (tono, entonación).
+          
+    Separador
+    #t_3_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 3.2 Atención y orientación al visitante
+
+    .p-4.bg-acento-contenido-a20.rounded-4.mb-4
+      p.mb-0 Los protocolos para la atención y orientación al cliente (huésped o visitante) deben ser conocidos por el personal que tenga contacto e interacción con ellos. Conocer estas reglas permite estar preparado ante las diferentes situaciones del servicio al cliente. En algunas ocasiones las empresas tienen normas de cortesía predeterminadas para cada situación, impartidas generalmente durante el proceso de entrenamiento del personal. En caso de no encontrarse definidas, se sugiere aplicar las normas básicas de trato con el cliente:
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-lg-7.mb-4.mb-lg-0
+        .p-4.bg-img.bg-img--11.mb-3.rounded-4
+          p.mb-0 Saludar con cortesía; si se conoce a la persona, dirigirse por su apellido.
+        .p-4.bg-img.bg-img--11.mb-3.rounded-4
+          p.mb-0 Indagar por la necesidad con preguntas abiertas como "¿en qué puedo ayudarle?".
+        .py-2.px-4.bg-img.bg-img--11.mb-3.rounded-4
+          p.mb-0 Responder a la solicitud. Si no se conoce la respuesta, buscar quién pueda resolver la inquietud. En hotelería, el principio de nunca decir "no" al cliente es una máxima del sector.
+        .p-4.bg-img.bg-img--11.rounded-4
+          p.mb-0 Al finalizar, despedirse cortésmente.
+      .col-sm-10.col-lg-5
+        figure
+          img(data-aos="fade-down-right", src="@/assets/curso/tema3/7.png", alt="")
+
 
 </template>
 
